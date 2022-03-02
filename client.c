7c68a82368e40dfd63948219d9dcd5aad4ca695d
Marc Gaspà <mgaspjoval2002@gmail.com>
2022-03-02 22:24:41 +0100

    initial

---
+++ client.c
@@ -0,0 +1,12 @@
+//
+// Created by Marc Gaspà Joval on 2/3/22.
+//
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+
